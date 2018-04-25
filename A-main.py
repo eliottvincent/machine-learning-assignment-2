18a0a62b628ccade0523af11b29a068421409794
Eliott Vincent <eliott.20100@gmail.com>
2018-04-25 23:20:38 +0100

    main exemple

---
+++ A-main.py
@@ -0,0 +1,25 @@
+__author__ = "Simon Bonnaud, Arthur Chevallier, Anaïs Pignet, Eliott Vincent"
+__license__ = "MIT"
+__version__ = "0.1"
+
+#================================================================================
+# modules
+#================================================================================
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.cross_validation import cross_val_score
+
+
+#================================================================================
+# properties
+#================================================================================
+
+
+
+def main():
+	
+
+# program launch
+if __name__ == '__main__':
+	main()
