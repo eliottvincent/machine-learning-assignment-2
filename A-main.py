769d9b4f17998cdaa4f8081070afb583b069cf85
Eliott Vincent <eliott.20100@gmail.com>
2018-04-25 23:38:55 +0100

    added load_dataframe() and write_dataframe()

--- A-main.py
+++ A-main.py
@@ -18,8 +18,46 @@ from sklearn.cross_validation import cross_val_score
 def main():
 	return None
 
+
+
+
+#  ██╗███╗   ██╗██████╗ ██╗   ██╗████████╗    ██████╗ ██╗   ██╗████████╗██████╗ ██╗   ██╗████████╗
+#  ██║████╗  ██║██╔══██╗██║   ██║╚══██╔══╝   ██╔═══██╗██║   ██║╚══██╔══╝██╔══██╗██║   ██║╚══██╔══╝
+#  ██║██╔██╗ ██║██████╔╝██║   ██║   ██║█████╗██║   ██║██║   ██║   ██║   ██████╔╝██║   ██║   ██║   
+#  ██║██║╚██╗██║██╔═══╝ ██║   ██║   ██║╚════╝██║   ██║██║   ██║   ██║   ██╔═══╝ ██║   ██║   ██║   
+#  ██║██║ ╚████║██║     ╚██████╔╝   ██║      ╚██████╔╝╚██████╔╝   ██║   ██║     ╚██████╔╝   ██║   
+#  ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝    ╚═╝       ╚═════╝  ╚═════╝    ╚═╝   ╚═╝      ╚═════╝    ╚═╝   
+
+
+def load_dataframe(fileName):
+	"""load_dataframe
+	Loads a DataFrame from a .csv file.
+
+    Input:
+    fileName -- the name of the file (should be located in ./data/)
+    
+	Output:
+	pd.read_csv() -- the DataFrame loaded by Pandas
+    """
+	path = dataPath + fileName
+	return pd.read_csv(path, header=None, names=featuresNames)
+
+
+def write_dataframe(df, fileName):
+	"""write_dataframe
+	Writes a DataFrame into a .csv file.
+
+    Input:
+    df -- the DataFrame to write
+    fileName -- the name of the file (will be saved in ./data/)
+    """
+	path = dataPath + fileName
+	df.to_csv(path)
+
+
 def dataframeToNumpy(df):
 	return df[df.columns.values].values
+
 
 # program launch
 if __name__ == '__main__':
