65688051d0271d1c48e663ae7d7733a333002fd3
Eliott Vincent <eliott.20100@gmail.com>
2018-04-26 12:07:37 +0100

    first test with RandomForestClassifier()

--- A-main.py
+++ A-main.py
@@ -8,7 +8,13 @@ __version__ = "0.1"
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.cross_validation import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_classification
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 
 #================================================================================
@@ -18,12 +24,62 @@ dataPath = './data/'
 featuresNames = ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points', 'Wilderness_Area', 'Soil_Type', 'Cover_Type']
 
 def main():
-	df = load_dataframe('DataSet-cleaned-integer.csv')
 
-	convertedDf = dataframeToNumpy(df)
-	# TODO: split the df to get a training set and a test set
-	# scikit can do this easily with http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+	fullDf = load_dataframe('DataSet-cleaned-integer.csv')
+	trainDf, testDf = splitDataFrame(fullDf, 90)
 
+	# convertedDf = dataframeToNumpy(df)
+
+	RunRandomForestClassifier(fullDf, trainDf, testDf)
+	# RunExtraTreesClassifier(trainDf, testDf)
+
+
+	return None
+
+
+
+
+#   ██████╗██╗      █████╗ ███████╗███████╗██╗███████╗██╗███████╗██████╗ ███████╗
+#  ██╔════╝██║     ██╔══██╗██╔════╝██╔════╝██║██╔════╝██║██╔════╝██╔══██╗██╔════╝
+#  ██║     ██║     ███████║███████╗███████╗██║█████╗  ██║█████╗  ██████╔╝███████╗
+#  ██║     ██║     ██╔══██║╚════██║╚════██║██║██╔══╝  ██║██╔══╝  ██╔══██╗╚════██║
+#  ╚██████╗███████╗██║  ██║███████║███████║██║██║     ██║███████╗██║  ██║███████║
+#   ╚═════╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+
+
+def RunRandomForestClassifier(fullDf, trainDf, testDf):
+	
+	# Load the training and test data sets
+	train = load_dataframe('DataSet-cleaned-integer.csv')
+	test = load_dataframe('DataSet-1000-cleaned.csv')
+
+	# Create numpy arrays for use with scikit-learn
+	# train_X = train.drop(['Id','Cover_Type'],axis=1).values
+	train_X = train.drop(['Cover_Type'], axis=1).values	# sample
+	train_y = train.Cover_Type.values						# target feature (to predict)
+	# test_X = test.drop('Id',axis=1).values
+	test_X = test.values
+
+	# Split the training set into training and validation sets
+	X, X_, y, y_ = train_test_split(train_X, train_y, test_size=0.2)
+
+	# Train and predict with the random forest classifier
+	rf = RandomForestClassifier()
+	rf.fit(X, y)
+	y_rf = rf.predict(X_)
+	print(metrics.classification_report(y_, y_rf))
+	print(metrics.accuracy_score(y_, y_rf))
+
+	# Retrain with entire training set and predict test set.
+	rf.fit(train_X, train_y)
+	y_test_rf = rf.predict(test_X)
+
+	# Write to CSV
+	fullDf.DataFrame({'Cover_Type': y_test_rf}).sort_index(ascending=False, axis=1).to_csv('rf1.csv', index=False)
+	return None
+
+
+def RunExtraTreesClassifier():
 	et = ExtraTreesClassifier(n_estimators=100, max_depth=None, random_state=0)
 
 	columns = ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points']
@@ -35,7 +91,6 @@ def main():
 	et_score = cross_val_score(et, features, labels, n_jobs=-1).mean()
 
 	print("{0} -> ET: {1})".format(columns, et_score))
-	return None
 
 
 
@@ -74,9 +129,28 @@ def write_dataframe(df, fileName):
 	df.to_csv(path)
 
 
+
+
+#  ██╗   ██╗████████╗██╗██╗     ███████╗
+#  ██║   ██║╚══██╔══╝██║██║     ██╔════╝
+#  ██║   ██║   ██║   ██║██║     ███████╗
+#  ██║   ██║   ██║   ██║██║     ╚════██║
+#  ╚██████╔╝   ██║   ██║███████╗███████║
+#   ╚═════╝    ╚═╝   ╚═╝╚══════╝╚══════╝
+
+
 def dataframeToNumpy(df):
 	return df[df.columns.values].values
 
+
+def splitDataFrame(fullDf, trainPercentage):
+	test_size = (100 - trainPercentage) / 100
+	trainDf, testDf = train_test_split(fullDf, test_size=test_size)
+
+	print('Original: ' + str(fullDf.size) + ' / ' + str(fullDf.shape))
+	print('Train: ' + str(trainDf.size) + ' / ' + str(trainDf.shape))
+	print('Test: ' + str(testDf.size) + ' / ' + str(testDf.shape))
+	return trainDf, testDf
 
 # program launch
 if __name__ == '__main__':
